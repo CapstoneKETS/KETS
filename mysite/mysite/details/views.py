c83482fb13077e65d14e84e9397e7897d14db53c
geonparkpark <150771369+geonparkpark@users.noreply.github.com>
2023-12-05 03:51:30 +0900

    first

--- mysite/mysite/details/views.py
+++ mysite/mysite/details/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import newsData
 
 def index(request):
     return render(request, 'details/index.html')
 # Create your views here.
+
+def newsDataView(request):
+    news_data_all = newsData.objects.all()
+    return render(request, 'details/index.html', {'news_data_list': news_data_all})
