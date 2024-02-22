898a66dc9d57470e7a68427aafadfde48247ed42
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2024-02-22 15:40:40 +0900

    Revert "Chea dan bi"

--- mysite/mysite/mainpage/views.py
+++ mysite/mysite/mainpage/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-from .models import kwRank
+
 def index(request):
-    queryset = kwRank.objects.order_by('-rank')[:16]
-
-    keyword_list = [obj.keyword for obj in queryset]
-
-    context = {
-        'keyword_list': keyword_list,
-    }
-
     return render(request, 'mainpage/index.html')
 
 
